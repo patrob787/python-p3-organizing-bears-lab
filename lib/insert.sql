132223b21cc4eef2d129a12a02a429e6fb1c4302
Patrick Roberts <patrob22@gmail.com>
2023-05-05 00:23:37 -0400

    lab complete

---
+++ lib/insert.sql
@@ -0,0 +1,29 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Mr. Chocolate", 5, "Male", "Black", "Hungry", True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Rowdy", 2, "Male", "Brown", "Rowdy", True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Tabitha", 10, "Female", "Brown", "Gentle", True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Sergeant Brown", 17, "Male", "Amber", "Wise", False);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Melissa", 1, "Female", "Black", "Mischievious", True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Grinch", 7, "Female", "Amber", "Grumpy", False);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Wendy", 3, "Female", "White", "Curious", True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (NULL, 6, "Male", "Brown", NULL, False);
+
+
+
+
+
+
